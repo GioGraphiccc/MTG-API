257d11c585abe0834944f5f77b67335c0fb8f300
Gio <giovherr@gmail.com>
2023-05-20 14:00:23 -0400

    Actual merge ig.
    I made a mistake earlier... good thing I had a backup.

---
+++ pages/Card Analyzer.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import requests
+
+from methods.tools import formatWord, collectResponses
+from methods.database_methods import addCard
+
+def CardAnalyzer():
+    baseUrl = "https://api.scryfall.com"
+    card_name = st.text_input("Enter card name")
+    results = ""
+    if(card_name): 
+        url = baseUrl + "/cards/search"
+        response = requests.get(url, params={'q':card_name}).json()
+        results = collectResponses(response)
+
+    if results != "":
+        selected_set = st.selectbox(
+        'Results',
+        results)
+        url = baseUrl + "/cards/" + results[selected_set]
+        response = requests.get(url).json()
+        st.divider()
+        #st.write(response)                                             
+        legalityS = response['legalities']['standard']
+        legalityC = response['legalities']['commander']
+        price = response['prices']['usd']
+        priceF = response['prices']['usd_foil']
+        rarity = response['rarity']
+        set_name  = response['set_name']
+        addCard(response['name'], set_name, price)
+        col1, col2 = st.columns(2)
+        with col1:
+            #st.write(response)
+            st.write(response['name'])
+            st.write("Standard: " + formatWord(legalityS))
+            st.write("Commander: " + formatWord(legalityC))
+            st.write("Rarity: " + rarity)
+            st.write("Set: " + set_name)
+            if(price is None):
+                st.write("Price: N/A")
+            else:
+                st.write("Price: $" + price)
+            if(priceF is None):
+                st.write("Foil Price: N/A")
+            else:
+                st.write("Foil Price: $" + priceF)
+
+        with col2:
+            doubleface = False
+            replace = st.empty()
+            replaceEnlarge = st.empty()
+            try:
+                if(len(response['card_faces']) != 1):
+                    doubleface = True
+            except:
+                doubleface = False
+            enlarge = st.checkbox('Enlarge')
+            if(doubleface):
+                replaceEnlarge.empty()
+                with replace.container():
+                    st.image(response['card_faces'][0]['image_uris']['small'])
+                    st.image(response['card_faces'][1]['image_uris']['small'])
+                
+                if doubleface and enlarge:
+                    replace.empty()
+                    with replaceEnlarge.container():
+                        st.image(response['card_faces'][0]['image_uris']['normal'])
+                        st.image(response['card_faces'][1]['image_uris']['normal'])
+            else:
+                replaceEnlarge.empty()
+                with replace.container():
+                    st.image(response['image_uris']['small'])
+                if(enlarge):
+                    replace.empty()
+                    with replaceEnlarge.container():
+                        st.image(response['image_uris']['normal'])
+
+st.set_page_config(page_title="MTG Analyzer", page_icon=":shield:")
+st.title("MTG Card and Set Analyzer - Scryfall API")
+CardAnalyzer()
---
+++ pages/Deck Builder.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+from methods.tools import parsePastedText, display_images_price_stats
+
+
+def DeckBuilder():
+    baseUrl = "https://api.scryfall.com"
+    pastedText = st.text_area("Paste a deck")
+    if(pastedText):
+        users_deck = parsePastedText(pastedText, baseUrl)
+        st.write(users_deck)
+        display_images_price_stats({'data':users_deck})
+
+
+    
+st.set_page_config(page_title="MTG Analyzer", page_icon=":shield:")
+st.title("MTG Card and Set Analyzer - Scryfall API")
+DeckBuilder()
